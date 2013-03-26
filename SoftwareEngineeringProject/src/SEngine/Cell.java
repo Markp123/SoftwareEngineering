0f67dc87e9b3d31c8ef91a3cf766e9e7059f8fbb
Markp123 <markp.patching@gmail.com>
2013-03-26 22:31:23 +0000

    Playing about with a hex print
    Signed-off-by: Markp123 <markp.patching@gmail.com>

--- SoftwareEngineeringProject/src/SEngine/Cell.java
+++ SoftwareEngineeringProject/src/SEngine/Cell.java
@@ -1,5 +1,16 @@
 package SEngine;
 
 public class Cell {
-
+	
+	public String image;
+	
+	public Cell()
+	{
+		image = "f";
+	}
+	
+	public String toString()
+	{
+		return ".";
+	}
 }
--- SoftwareEngineeringProject/src/SEngine/WorldModel.java
+++ SoftwareEngineeringProject/src/SEngine/WorldModel.java
@@ -1,5 +1,44 @@
 package SEngine;
 
 public class WorldModel {
-
+	private Cell[][] world;
+	
+	public WorldModel()
+	{
+		world = new Cell[20][20];
+		
+		for (int i = 0; i < world.length; i++)
+        {                
+            for(int j = 0; j < world.length; j++)
+            {
+                Cell cell = new Cell();
+                world[i][j] = cell;
+            }
+        }
+	}
+	
+	public static void main(String[] args)
+	{
+		WorldModel wm = new WorldModel();
+		wm.printWorld();
+	}
+	
+	public void printWorld()
+	{
+		boolean hex = false;
+		for (int i = 0; i < world.length; i++)
+		{
+			for (int j = 0; j < world.length; j++)
+			{
+				if (hex)
+				{
+					System.out.print(" ");
+					System.out.print(world[i][j].toString());
+					System.out.print("|");
+				}
+			}
+			System.out.println("");
+			hex = !hex;
+		}
+	}
 }
