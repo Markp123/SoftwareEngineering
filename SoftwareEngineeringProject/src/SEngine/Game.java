db6821881a112465ec72a88de1557cc9e8cc84fa
kt225 <kt225@INF02900139.ad.susx.ac.uk>
2013-04-05 15:57:29 +0100

    game missing ant/markers implementation

--- SoftwareEngineeringProject/src/SEngine/Game.java
+++ SoftwareEngineeringProject/src/SEngine/Game.java
@@ -1,6 +1,4 @@
 package SEngine;
-import Ant;
-import Colour;
 
 import java.util.Random;
 
@@ -51,7 +49,8 @@ public class Game {
 		}
 	}
 	private void set_food_at(int[] p, int food){
-		
+		world.getCell(p[0], p[1]).setFood(true);
+		world.getCell(p[0], p[1]).setFoodAmount(food);
 	}
 	private void set_marker_at(int[] p, Colour colour, int i){
 		
@@ -65,7 +64,9 @@ public class Game {
     private void set_ant_at(int[] newp, Ant a){
     	
     }
-
+	private boolean some_ant_is_at(int[] newp){
+		return false;
+	}
     private int adjacent_ants(int[] p, Colour c){
     	return 0;//returns amount of enemy ants around the ant in this position
     }
@@ -124,13 +125,7 @@ public class Game {
 	    	check_for_surrounded_ant_at(adjacent_cell(p,d));
 	    }
     }
-    	      
-	private boolean some_ant_is_at(int[] newp){
-		return false;
-	}
-	private Instruction get_instruction(int BrainNum, int state){
-		return;
-	}
+	
 	private void step(int id){
 	  if (ant_is_alive(id)){
 	    int[] p = find_ant(id);
@@ -139,76 +134,87 @@ public class Game {
 	      a.setResting(a.getResting()-1);
 	    }
 	    else{
-	    	Instruction instr = get_instruction(a.getBrainNum(), a.getState());
+	    	Instruction instr = a.getInstruction();
 	    	String name = instr.getClass().getName();
 	    	if(name.equals("Sense")){
-	    		int[] p2 = sensed_cell(p, a.getDirection(), instr.getDirection());
+	    		Sense instr2 = (Sense)a.getInstruction();
+	    		int[] p2 = sensed_cell(p, a.getDirection(), instr2.getDirection());
 		        int st;
-		        if(cell_matches(p2, instr.getCondition, a.getColour())){
-		        	st = instr.getState1;
+		        if(cell_matches(p2, instr2.getCondition(), a.getColour())){
+		        	st = instr2.getState1();
 		      	}
 		        else{
-		        	st = instr.getState2;
+		        	st = instr2.getState2();
 		        }
 		        a.setState(st);
 	    	}
 	    	else if(name.equals("Mark")){
-	    		set_marker_at(p, a.getColour(), instr.getMarker());
-	        	a.setState(instr.getState());
+	    		Mark instr2 = (Mark)a.getInstruction();
+	    		set_marker_at(p, a.getColour(), instr2.getMarker());
+	        	a.setState(instr2.getState());
 	    	}
 	    	else if(name.equals("Unmark")){
-	    		clear_marker_at(p, a.getColour(), instr.getMarker());
-	        	a.setState(instr.getState());
+	    		Unmark instr2 = (Unmark)a.getInstruction();
+	    		clear_marker_at(p, a.getColour(), instr2.getMarker());
+	        	a.setState(instr2.getState());
 	    	}
 	    	else if(name.equals("PickUp")){
+	    		PickUp instr2 = (PickUp)a.getInstruction();
 	    		if(a.isHas_food() || food_at(p) == 0){
-	        		a.setState(instr.getState2());
+	        		a.setState(instr2.getState2());
 	        	}
 	        	else{
 	        		set_food_at(p, food_at(p) - 1);
 	            	a.setHas_food(true);
-	            	a.setState(instr.getState1());
+	            	a.setState(instr2.getState1());
 	        	}
 	    	}
 	    	else if(name.equals("Drop")){
+	    		Drop instr2 = (Drop)a.getInstruction();
 	    		 if(a.isHas_food()){
 				        set_food_at(p, food_at(p) + 1);
 				        a.setHas_food(false);
-				        a.setState(instr.getState());
+				        a.setState(instr2.getState());
 			        }
 	    	}
 	    	else if(name.equals("Turn")){
-	    		setDirection(a, turn(instr.getDirection(), a.getDirection()));
-	        	a.setState(instr.getState());
+	    		Turn instr2 = (Turn)a.getInstruction();
+	    		a.setDirection(Turn(instr2.getDirection(), a.getDirection()));
+	        	a.setState(instr2.getState());
 	    	}
 	    	else if(name.equals("Move")){
+	    		Move instr2 = (Move)a.getInstruction();
 	    		int[] newp = adjacent_cell(p, a.getDirection());
 		        if(rocky(newp) || some_ant_is_at(newp)){
-		            set_state(a, instr.getState2());
+		            a.setState(instr2.getState2());
 		        }
 		        else{
 		            clear_ant_at(p);
 		            set_ant_at(newp, a);
-		            a.setState(instr.getState1());
+		            a.setState(instr2.getState1());
 		            a.setResting(14);
 		            check_for_surrounded_ants(newp);
 		        }
 	    	}
 	    	else if(name.equals("Flip")){
+	    		Flip instr2 = (Flip)a.getInstruction();
 	    		int st;
-		        if(randomint(instr.getP()) == 0){
-		        	  st = instr.getState1();
+		        if(randomint(instr2.getP()) == 0){
+		        	  st = instr2.getState1();
 		        }
 		        else{
-		        	st = instr.getState2();
+		        	st = instr2.getState2();
 		        }
 		        a.setState(st);
 	    	}
-	      }
 	    }
 	  }
 	}
 	
+	private boolean cell_matches(int[] p2, Condition condition, Colour colour) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 	private int[] sensed_cell(int[] p, int direction, Direction sense_dir){
 		int[] x = new int[2];
 		switch(sense_dir){
