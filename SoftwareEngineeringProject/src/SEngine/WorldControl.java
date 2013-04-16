6fda786c1facfb19aa2d71a3361e69bf4c133d8b
Markp123 <markp.patching@gmail.com>
2013-04-16 19:42:55 +0100

    Added a main menu. Need to implement it to change in game variables

--- SoftwareEngineeringProject/src/SEngine/WorldControl.java
+++ SoftwareEngineeringProject/src/SEngine/WorldControl.java
@@ -1,7 +1,10 @@
 package SEngine;
 
 import java.awt.event.*;
-import java.awt.Dialog;
+import java.awt.*;
+import java.io.File;
+
+import javax.swing.*;
 
 public class WorldControl extends MouseAdapter
 {
@@ -13,48 +16,135 @@ public class WorldControl extends MouseAdapter
 		this.view = view;
 		this.world = world;
 
-		for(int i = 0; i < cells.length;i++) 
-		{
-			for(int k=0; k < cells[i].length;k++) 
-			{
-				final int row = i;
-				final int col = k;
-				cells[i][k].addMouseListener(new MouseListener(){
-
-					public void mouseClicked(MouseEvent e) {
-
-						if (e.getButton() == MouseEvent.BUTTON1) 
-						{
-							//left mouse click
-						}
-						else if (e.getButton() == MouseEvent.BUTTON3) 
-						{
-							//right mouse click
-						
-						}
-					}
-
-					public void mouseEntered(MouseEvent e) 
-					{
-
-					}
-
-					public void mouseExited(MouseEvent e) 
-					{
-
-					}
-
-					public void mousePressed(MouseEvent e) 
-					{
-
-					}
-
-					public void mouseReleased(MouseEvent e) 
-					{
-
-					}
-				});
+		view.getGame().addMouseListener(new MouseListener(){
+			public void mouseClicked(MouseEvent e) {
+				view.showGame();
 			}
+			public void mouseEntered(MouseEvent e){
+			}
+			public void mouseExited(MouseEvent e){
+			}
+			public void mousePressed(MouseEvent e){
+			}
+			public void mouseReleased(MouseEvent e){
+			}
+		});
+
+		view.getTournament().addMouseListener(new MouseListener(){
+			public void mouseClicked(MouseEvent e){
+				view.chooseAmountOfPlayers();
+			}
+			public void mouseEntered(MouseEvent e){
+			}
+			public void mouseExited(MouseEvent e){
+			}
+			public void mousePressed(MouseEvent e){
+			}
+			public void mouseReleased(MouseEvent e){
+
+			}
+		});
+
+		view.getPlayer1().addMouseListener(new MouseListener(){
+			public void mouseClicked(MouseEvent e) {
+				JFileChooser p1Chooser = new JFileChooser();
+				int returnValue = p1Chooser.showOpenDialog(null);
+				if (returnValue == JFileChooser.APPROVE_OPTION) {
+					File selectedFile = p1Chooser.getSelectedFile();
+					//Add selected file to tournament
+					view.choice1.setText(selectedFile.getName());
+				}
+			}
+			public void mouseEntered(MouseEvent e){
+			}
+			public void mouseExited(MouseEvent e){
+			}
+			public void mousePressed(MouseEvent e){
+			}
+			public void mouseReleased(MouseEvent e){
+			}
+		});
+
+		view.getPlayer2().addMouseListener(new MouseListener(){
+			public void mouseClicked(MouseEvent e) {
+				JFileChooser p2Chooser = new JFileChooser();
+				int returnValue = p2Chooser.showOpenDialog(null);
+				if (returnValue == JFileChooser.APPROVE_OPTION) {
+					File selectedFile = p2Chooser.getSelectedFile();
+					//Add selected file to tournament
+					view.choice2.setText(selectedFile.getName());
+				}
+			}
+			public void mouseEntered(MouseEvent e){
+			}
+			public void mouseExited(MouseEvent e){
+			}
+			public void mousePressed(MouseEvent e){
+			}
+			public void mouseReleased(MouseEvent e){
+
+			}
+		});
+
+		view.choosen.addMouseListener(new MouseListener(){
+			public void mouseClicked(MouseEvent e) {
+				view.setNumOfPlayers(Integer.parseInt(view.NumOfPlayers.getText()));
+				view.showTournament();
+			}
+			public void mouseEntered(MouseEvent e){
+			}
+			public void mouseExited(MouseEvent e){
+			}
+			public void mousePressed(MouseEvent e){
+			}
+			public void mouseReleased(MouseEvent e){
+			}
+		});
+
+		for (int i = 0; i < view.getAmountOfPlayers(); i ++)
+		{
+			view.getButtonArray().get(i).addMouseListener(new MouseListener(){
+				public void mouseClicked(MouseEvent e) {
+					JButton button = (JButton)e.getSource();
+					String buttonName = button.getName();
+					JFileChooser brainChooser = new JFileChooser();
+					int returnValue = brainChooser.showOpenDialog(null);
+					if (returnValue == JFileChooser.APPROVE_OPTION) {
+						File selectedFile = brainChooser.getSelectedFile();
+						//Add selected file to tournament
+						view.getTextFieldArray().get(Integer.parseInt(buttonName)).setText(selectedFile.getName());
+					}
+				}
+				public void mouseEntered(MouseEvent e){
+				}
+				public void mouseExited(MouseEvent e){
+				}
+				public void mousePressed(MouseEvent e){
+				}
+				public void mouseReleased(MouseEvent e){
+				}
+			});
 		}
+
+
+		view.getStart().addMouseListener(new MouseListener(){
+			public void mouseClicked(MouseEvent e){
+				System.out.println("Need to implement the actual start");
+			}
+			public void mouseEntered(MouseEvent e){
+			}
+			public void mouseExited(MouseEvent e){
+			}
+			public void mousePressed(MouseEvent e){
+			}
+			public void mouseReleased(MouseEvent e){
+			}
+		});
 	}
 }
+
+
+
+
+
+
--- SoftwareEngineeringProject/src/SEngine/WorldGUI.java
+++ SoftwareEngineeringProject/src/SEngine/WorldGUI.java
@@ -1,9 +1,10 @@
 package SEngine;
 
 import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.swing.*;
 
 /**
@@ -13,8 +14,31 @@ import javax.swing.*;
  */
 public class WorldGUI extends JFrame
 {
+	ImageIcon icon = new ImageIcon("C:/Users/Mark/Desktop/ant_image.png");
+	JFrame main = new JFrame();
+	JLabel imageLabel = new JLabel(icon);
+	JPanel imagePanel = new JPanel();
+	JFrame twoPGame = new JFrame();
+	JPanel twoPGamePanel = new JPanel();
+	JFrame tournamentFrame = new JFrame();
+	JPanel tournamentPanel = new JPanel();
+	JFrame amountOfPlayersFrame = new JFrame();
+	JPanel numOfP = new JPanel();
+	JButton game = new JButton("Single Game");
+	JButton tournament = new JButton("Tournament");
+	JTextField choice1 = new JTextField();
+	JTextField choice2 = new JTextField();
+	JTextField NumOfPlayers = new JTextField();
+	JButton player1 = new JButton("Player 1");
+	JButton player2 = new JButton("Player 2");
+	JButton choosen = new JButton("Confirm");
+	JButton start = new JButton("START");
+	private List<JButton> buttonArray = new ArrayList<JButton>();
+	private List<JTextField> textFieldArray = new ArrayList<JTextField>();
 	private JPanel board = new JPanel();
 	private CellRep[][] cellArray;
+	private int amountOfPlayers;
+
 
 	/**
 	 * Constructor for the GUI
@@ -24,14 +48,145 @@ public class WorldGUI extends JFrame
 	 *
 	 * @param world - the state of the world
 	 */
-	public WorldGUI(World world)
+	public WorldGUI()
+	{
+		main.getContentPane().setLayout(new BorderLayout());
+		imagePanel.add(imageLabel);
+		main.add(game, BorderLayout.CENTER);
+		main.add(tournament, BorderLayout.SOUTH);
+		main.add(imagePanel, BorderLayout.NORTH);
+		main.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		main.pack();
+		main.setVisible(true);
+	}
+	
+	public void showGame()
+	{
+		main.dispose();
+		twoPGame.getContentPane().setLayout(new BorderLayout());
+		twoPGamePanel.setLayout(new GridLayout(2,2));
+		twoPGamePanel.add(player1);
+		twoPGamePanel.add(choice1);
+		choice1.setEditable(false);
+		twoPGamePanel.add(player2);
+		twoPGamePanel.add(choice2);
+		choice2.setEditable(false);
+		twoPGame.add(twoPGamePanel, BorderLayout.CENTER);
+		twoPGame.add(start, BorderLayout.SOUTH);
+		twoPGame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		twoPGame.pack();
+		twoPGame.setVisible(true);
+	}
+	
+	public void chooseAmountOfPlayers()
+	{
+		main.dispose();
+		numOfP.setLayout(new GridLayout(1,2));
+		amountOfPlayersFrame.getContentPane().setLayout(new BorderLayout());
+		numOfP.add(new JLabel("Input number of players:"));
+		numOfP.add(NumOfPlayers);
+		amountOfPlayersFrame.add(numOfP, BorderLayout.CENTER);
+		amountOfPlayersFrame.add(choosen, BorderLayout.SOUTH);
+		amountOfPlayersFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		amountOfPlayersFrame.pack();
+		amountOfPlayersFrame.setVisible(true);
+	}
+	
+	public void showTournament()
+	{
+		amountOfPlayersFrame.dispose();
+		for (int i = 0; i < amountOfPlayers; i++)
+		{
+			buttonArray.add(new JButton("Player"+(i+1)));
+			buttonArray.get(i).setName("" + i);
+			buttonArray.get(i).addMouseListener(new MouseListener(){
+				public void mouseClicked(MouseEvent e) {
+					JButton button = (JButton)e.getSource();
+					String buttonName = button.getName();
+					JFileChooser brainChooser = new JFileChooser();
+					int returnValue = brainChooser.showOpenDialog(null);
+					if (returnValue == JFileChooser.APPROVE_OPTION) {
+						File selectedFile = brainChooser.getSelectedFile();
+						//Add selected file to tournament
+						textFieldArray.get(Integer.parseInt(buttonName)).setText(selectedFile.getName());
+					}
+				}
+				public void mouseEntered(MouseEvent e){
+				}
+				public void mouseExited(MouseEvent e){
+				}
+				public void mousePressed(MouseEvent e){
+				}
+				public void mouseReleased(MouseEvent e){
+				}
+			});
+		}
+		for (int i = 0; i < amountOfPlayers; i++)
+		{
+			textFieldArray.add(new JTextField()); 
+		}
+		tournamentFrame.getContentPane().setLayout(new BorderLayout());
+		tournamentPanel.setLayout(new GridLayout(amountOfPlayers, 2));
+		for (int i = 0; i < amountOfPlayers; i++)
+		{
+			tournamentPanel.add(buttonArray.get(i));
+			tournamentPanel.add(textFieldArray.get(i));
+			textFieldArray.get(i).setEditable(false);
+		}
+		tournamentFrame.add(tournamentPanel, BorderLayout.CENTER);
+		tournamentFrame.add(start, BorderLayout.SOUTH);
+		tournamentFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		tournamentFrame.pack();
+		tournamentFrame.setVisible(true);
+	}
+	
+	public List<JTextField> getTextFieldArray() {
+		return textFieldArray;
+	}
+
+	public int getAmountOfPlayers() {
+		return amountOfPlayers;
+	}
+
+	public List<JButton> getButtonArray() {
+		return buttonArray;
+	}
+
+	public void setNumOfPlayers(int number){
+		this.amountOfPlayers = number;
+	}
+	
+	public JButton getStart() {
+		return start;
+	}
+
+	public JFrame getTwoPGame()
+	{
+		return twoPGame;
+	}
+
+	public JButton getGame(){
+		return game;
+	}
+	public JButton getTournament(){
+		return tournament;
+	}
+	
+	public JButton getPlayer1() {
+		return player1;
+	}
+
+	public JButton getPlayer2() {
+		return player2;
+	}
+
+	public void setUpGameWorld(World world)
 	{
 		cellArray = new CellRep[world.getRows()][world.getColumns()];
 		this.getContentPane().setLayout(new BorderLayout());
 		board.setLayout(new GridLayout(world.getRows(), world.getColumns()));
 		for (int i = 0; i < world.getRows(); i++)
 		{
-			
 			for (int j = 0; j < world.getColumns(); j++)
 			{
 				CellRep c = new CellRep(world.getCell(i, j));
@@ -75,8 +230,8 @@ public class WorldGUI extends JFrame
 	public static void main(String[] args)
 	{
 		World world = new World(150, 150);
-		WorldGUI view = new WorldGUI(world);
-		//WorldControl control = new WorldControl(world, view);
+		WorldGUI view = new WorldGUI();
+		WorldControl control = new WorldControl(world, view);
 	}
 
 	class CellRep extends JLabel
